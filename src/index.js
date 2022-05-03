63e3597039447a5ebac475b5d71c04cf8cad0836
Andrei - Petru Balea <andrei.balea@student.upt.ro>
2022-05-03 09:35:46 +0300

    moved con in get(), might work if it's local

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ const PASSWORD = process.env.PASSWORD;
 const express = require('express');
 const db = require('mysql');
 
-var con = db.createConnection({
-    host: LOCALHOST,
-    user: "root",
-    password: PASSWORD,
-    database: "dexonline",
-});
+// var con = db.createConnection({
+//     host: LOCALHOST,
+//     user: "root",
+//     password: PASSWORD,
+//     database: "dexonline",
+// });
 
 const app = express()
 
@@ -30,32 +30,29 @@ app.get('/:myLetters', (req, res) => {
     }
     myLetters = myLetters.toLowerCase();
 
-    // start connection
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-
-        // querry the database
-        con.query("SELECT DISTINCT(TRIM(getFirstWord(description))) FROM Entry WHERE isValid(getFirstWord(description), \"" + myLetters + "\");", function (err, result, fields) {
-            if (err) throw err;
-            //res.json(result);
-            var words = [];
-            // for( var i = 0; i < result.length; i++){
-            //     words.push(result["getFirstWord(description)"]);
-            // }
-            Object.keys(result).forEach(function(key) {
-                words.push(result[key]["(TRIM(getFirstWord(description)))"]);
-            });
-            res.json({words: words});
-        });
-
-    });    
-    
-    // end connection
-    con.end(function(err) {
-        if (err) throw err;
-        console.log("Connection closed");
+    // create connection
+    var con = db.createConnection({
+        host: LOCALHOST,
+        user: "root",
+        password: PASSWORD,
+        database: "dexonline",
     });
+
+    // querry the database
+    con.query("SELECT DISTINCT(TRIM(getFirstWord(description))) FROM Entry WHERE isValid(getFirstWord(description), \"" + myLetters + "\");", function (err, result, fields) {
+        if (err) throw err;
+        //res.json(result);
+        var words = [];
+        // for( var i = 0; i < result.length; i++){
+        //     words.push(result["getFirstWord(description)"]);
+        // }
+        Object.keys(result).forEach(function(key) {
+            words.push(result[key]["(TRIM(getFirstWord(description)))"]);
+        });
+        res.json({words: words});
+    });
+
+    //con.destroy();
 });
 
-app.listen(PORT, () => console.log("server running on PORT" + PORT));
+app.listen(PORT, () => console.log("server running on PORT: " + PORT));
